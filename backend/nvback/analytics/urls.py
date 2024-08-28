0c545a6bedb8851fca27dc552a4a76dfedd9c4b0
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2024-08-28 11:26:31 +0600

    make custom admin page

--- backend/nvback/analytics/urls.py
+++ backend/nvback/analytics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from .views import *
+from .admin_views import *
 
 urlpatterns = [
+    path('page/', analytics_admin_view, name="analytics page"),
     path('send-message/', SendMessageAPIVIew.as_view(), name="Send Message"),
 ]
